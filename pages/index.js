3b7a46ccc17f1578b816ebbfe28d2ff49b6f6f1b
Otsile076 <125870039+Otsile076@users.noreply.github.com>
2023-09-12 09:52:10 +0200

    added fetch from supabase

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
-import { getFeaturedEvents } from '../dummy-data';
+import { getFeaturedEvents } from '../components/Helpers/API-util'
 import EventList from '../components/events/event-list';
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
-
+function HomePage(props) {
   return (
     <div>
-      <EventList items={featuredEvents} />
+      <EventList items={props.events} />
     </div>
   );
 }
 
-export default HomePage;
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+        featuredEvents: featuredEvents
+    }
+  }
+};
